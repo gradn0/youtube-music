723bb68fc822f4c24d28fd2b2bc598a0b9556085
Nathan Gradden <nathan.gradden@outlook.com>
2024-04-17 19:07:06 +0100

    barebones clip creation backend

--- client/src/components/ClipForm.tsx
+++ client/src/components/ClipForm.tsx
@@ -1,41 +1,91 @@
+import { SubmitHandler, useForm } from "react-hook-form"
+import { fetchFromAPI } from "../utils/fetchFromAPI";
+
+interface Fields {
+  title: string,
+  collection: string,
+  videoId: string,
+  startMins: number,
+  startSecs: number,
+  endMins: number,
+  endSecs: number
+}
+
 const ClipForm = () => {
+  const {register, handleSubmit} = useForm<Fields>();
+
+  const onSubmit: SubmitHandler<Fields> = (data) => {
+    const clip = {
+      title: data.title,
+      playlist: data.collection,
+      videoId: data.videoId,
+      start: (data.startMins*60*1000) + (data.startSecs*1000),
+      end: (data.endMins*60*1000) + (data.endSecs*1000)
+    }
+    console.log(clip)
+    fetchFromAPI("clips", "post", clip)
+    .then(json => console.log(json))
+  }
 
   return (
-    <form id="clipForm" className="flex flex-col bg-modalGray w-[20em] p-4 gap-4 rounded shadow-xl text-textGray absolute z-20 top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]">
+    <form 
+      id="clipForm" 
+      className="flex flex-col bg-modalGray w-[20em] p-4 gap-4 rounded shadow-xl text-textGray absolute z-20 top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]"
+      onSubmit={handleSubmit(onSubmit)}
+    >
+
       <h2 className="text-subheading">New Clip</h2>
       <input 
         type="text"
-        placeholder="Name"
+        placeholder="Title"
         className="input"
+        {...register("title", {required: true, maxLength: 40})}
       />
 
       <input 
         type="text"
         placeholder="Collection"
         className="input"
+        {...register("collection", {required: true, maxLength: 40})}
       />
 
       <input 
         type="text"
         placeholder="Video ID"
         className="input"
+        {...register("videoId", {required: true})}
       />
-      <div className="flex text-body text-textGray justify-between">
 
+      <div className="flex text-body text-textGray justify-between">
         <p className="">Interval</p>
 
         <div className="flex gap-1">
-          <input className="w-[4ch] input text-center" type="text"/>:
-          <input className="w-[4ch] input text-center" type="text"/>
+          <input 
+            className="w-[4ch] input text-center" 
+            type="text"
+            {...register("startMins", {required: true})}
+          />:
+          <input 
+            className="w-[4ch] input text-center" 
+            type="text"
+            {...register("startSecs", {required: true})}
+          />
 
           <p className="mx-2">to</p>
-
-          <input className="w-[4ch] input text-center" type="text"/>:
-          <input className="w-[4ch] input text-center" type="text"/>
+          <input 
+            className="w-[4ch] input text-center" 
+            type="text"
+            {...register("endMins", {required: true})}
+          />:
+          <input 
+            className="w-[4ch] input text-center" 
+            type="text"
+            {...register("endSecs", {required: true})}
+          />
         </div>
         
       </div>
-
+      <button type="submit">Create</button>
     </form>
   )
 }
--- client/src/utils/fetchFromAPI.ts
+++ client/src/utils/fetchFromAPI.ts
@@ -3,7 +3,10 @@ export const BASE_URL = "http://localhost:3000/api"
 export const fetchFromAPI = async (query: string, requestType: string, body?:any) => {
   const res = await fetch(`${BASE_URL}/${query}`, {
     method: requestType,
-    body: body, 
+    body: JSON.stringify(body), 
+    headers: {
+      "Content-Type": "application/json"
+    }
   });
   const json = await res.json();
   return json;
