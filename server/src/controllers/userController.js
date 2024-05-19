8b81dfbb4f935363a81fc5011c7fbe3438782bd1
Nathan Gradden <nathan.gradden@outlook.com>
2024-05-19 18:55:42 +0100

    jwt token creation

--- server/src/controllers/userController.js
+++ server/src/controllers/userController.js
@@ -1,10 +1,16 @@
-import { User } from "../models/user.js";
+import { User } from "../models/user.js"
+import jwt from "jsonwebtoken"
+
+const createWebToken = (id) => {
+  return jwt.sign({id}, process.env.JWT_SECRET, {expiresIn: '3d'});
+}
 
 export const signup = async (req, res) => {
   const {email, password} = req.body;
   try {
     const newUser = await User.signup(email, password);
-    res.status(201).json(newUser);
+    const token = createWebToken(newUser._id);
+    res.status(201).json({email, token});
   } catch (error) {
     res.status(400).json({error: error.message});
   }
